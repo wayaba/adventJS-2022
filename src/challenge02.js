23fd74f6b43d2498a61ce55defe9e7e99caebd58
Pablo Pedraza <wensel84@gmail.com>
2022-12-02 20:37:12 -0300

    agrego otra opcion de resolucion

--- src/challenge02.js
+++ src/challenge02.js
@@ -1,7 +1,7 @@
 //https://adventjs.dev/es/challenges/2022/2
 
 function countHours(year, holidays) {
-  
+  /*
   let cont = 0
   holidays.forEach(date=> {
     const currentDate = new Date(`${date}/${year}`);
@@ -10,13 +10,19 @@ function countHours(year, holidays) {
       cont = cont + 2
   })
   return cont
+  */
+  return holidays.reduce((current, date) => {
+    const currentDate = new Date(`${date}/${year}`);
+    const dayOfWeek = currentDate.getDay();
+    return dayOfWeek !== 0 && dayOfWeek !== 6 ? current + 2 : current;
+  }, 0);
 }
 
-const year = 2022
-const holidays = ['01/06', '04/01', '12/25'] // formato MM/DD
+const year = 2022;
+const holidays = ["01/06", "04/01", "12/25"]; // formato MM/DD
 
 // 01/06 es el 6 de enero, jueves. Cuenta.
 // 04/01 es el 1 de abril, un viernes. Cuenta.
 // 12/25 es el 25 de diciembre, un domingo. No cuenta.
 
-countHours(year, holidays) // 2 días -> 4 horas extra en el año
+countHours(year, holidays); // 2 días -> 4 horas extra en el año
