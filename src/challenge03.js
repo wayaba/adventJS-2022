7d9d518e43248b7840953e0c89ca2346c46518ae
Pablo Pedraza <wensel84@gmail.com>
2022-12-03 22:22:01 -0300

    challenge 03

--- src/challenge03.js
+++ src/challenge03.js
@@ -1,28 +1,23 @@
-//https://adventjs.dev/es/challenges/2022/2
+//https://adventjs.dev/es/challenges/2022/3
 
-function countHours(year, holidays) {
-  /*
-  let cont = 0
-  holidays.forEach(date=> {
-    const currentDate = new Date(`${date}/${year}`);
-    const dayOfWeek = currentDate.getDay()
-    if(dayOfWeek !==0 && dayOfWeek !== 6)
-      cont = cont + 2
-  })
-  return cont
-  */
-  return holidays.reduce((current, date) => {
-    const currentDate = new Date(`${date}/${year}`);
-    const dayOfWeek = currentDate.getDay();
-    return dayOfWeek !== 0 && dayOfWeek !== 6 ? current + 2 : current;
-  }, 0);
+function distributeGifts(packOfGifts, reindeers) {
+  const giftWeight = packOfGifts.reduce((current,gift) =>{
+    return current + gift.length
+  },0)
+  
+  const canCarry = reindeers.reduce((current,reindeer) =>{
+    return current + (reindeer.length * 2)
+  },0)
+  
+  return Math.floor(canCarry / giftWeight)
 }
 
-const year = 2022;
-const holidays = ["01/06", "04/01", "12/25"]; // formato MM/DD
 
-// 01/06 es el 6 de enero, jueves. Cuenta.
-// 04/01 es el 1 de abril, un viernes. Cuenta.
-// 12/25 es el 25 de diciembre, un domingo. No cuenta.
+const packOfGifts = ["book", "doll", "ball"]
+const reindeers = ["dasher", "dancer"]
 
-countHours(year, holidays); // 2 días -> 4 horas extra en el año
+// el pack de regalos pesa 4 + 4 + 4 = 12
+// los renos pueden llevar (2 * 6) + (2 * 6) = 24
+// por lo tanto, Santa Claus puede entregar 2 cajas de regalos
+
+distributeGifts(packOfGifts, reindeers) // 2
