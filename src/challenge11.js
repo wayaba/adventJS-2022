c90645f8830a4138a2e536ac1f5d30a6c802789b
Pablo Pedraza <wensel84@gmail.com>
2022-12-20 12:05:21 -0300

    ej 11

--- src/challenge11.js
+++ src/challenge11.js
@@ -1,37 +1,34 @@
 //https://adventjs.dev/es/challenges/2022/11
 
 function getCompleted(part, total) {
-  /*
-  dt1 = new Date("October 13, 2014 " + part);
-  dt2 = new Date("October 13, 2014 " + total);
+  const [hoursPart, minutesPart, secondsPart] = part.split(":");
+  const [hoursTotal, minutesTotal, secondsTotal] = total.split(":");
 
-  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
-  console.log(dt2.getTime());
+  const partInSeconds =
+    Number(hoursPart) * 3600 + Number(minutesPart) * 60 + Number(secondsPart);
+  const totalInSeconds =
+    Number(hoursTotal) * 3600 +
+    Number(minutesTotal) * 60 +
+    Number(secondsTotal);
 
-  console.log(dt1.getTime());
-
-  let pepe = (dt1.getTime() * 100) / dt2.getTime();
-  console.log(pepe);
-
-  const getFraction = (decimal) => {
-    for (var denominator = 1; (decimal * denominator) % 1 !== 0; denominator++);
-    return { numerator: decimal * denominator, denominator: denominator };
+  const gcd = (a, b) => {
+    while (b) {
+      const t = b;
+      b = a % b;
+      a = t;
+    }
+    return a;
   };
-  let aaa = 3 / 1;
-  console.log(getFraction(33,3));
-  //console.log(diff);
-  diff /= 60;
-  return Math.abs(Math.round(diff));
-  */
+
+  const den = gcd(partInSeconds, totalInSeconds);
+  const numerator = partInSeconds / den;
+  const denominator = totalInSeconds / den;
+  return `${numerator}/${denominator}`;
 }
 
 getCompleted("01:00:00", "03:00:00"); // '1/3'
-/*
 getCompleted("02:00:00", "04:00:00"); // '1/2'
 getCompleted("01:00:00", "01:00:00"); // '1/1'
 getCompleted("00:10:00", "01:00:00"); // '1/6'
 getCompleted("01:10:10", "03:30:30"); // '1/3'
 getCompleted("03:30:30", "05:50:50"); // '3/5
-*/
-//3600+600+10 = 4210 --- 10800+1800+30 = 12630
-//10800+1800+30 = 12630 --- 18000+3000+50 = 21050
